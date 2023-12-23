0bdf7231d914c42cb0d7be7a6c37b8b4d74aa439
mayuryalij <yalijmayur@gmail.com>
2023-12-23 11:28:44 +0530

    commit

--- 1.Personal Blog Api/server/src/routes/s3Routes.js
+++ 1.Personal Blog Api/server/src/routes/s3Routes.js
@@ -3,6 +3,7 @@ const aws = require("aws-sdk");
 const multer = require("multer");
 const multerS3 = require("multer-s3");
 const s3Controller = require("../controllers/s3Controller");
+const { authenticate } = require("../middleware/authMiddleware");
 const dotenv = require("dotenv");
 dotenv.config();
 
@@ -21,14 +22,14 @@ const upload = multer({
     bucket: BUCKET,
     key: function (req, file, cb) {
       console.log(file);
-      cb(null, file.originalname);
+      cb(null, file.originalname); 
     },
   }),
 });
 
-router.post("/upload", upload.single("file"), s3Controller.uploadFile);
-router.get("/list", s3Controller.listFiles);
-router.get("/download/:filename", s3Controller.downloadFile);
-router.delete("/delete/:filename", s3Controller.deleteFile);
+router.post("/upload", authenticate, upload.single("file"), s3Controller.uploadFile);
+router.get("/list", authenticate, s3Controller.listFiles);
+router.get("/download/:filename", authenticate, s3Controller.downloadFile);
+router.delete("/delete/:filename", authenticate, s3Controller.deleteFile);
 
 module.exports = router;
